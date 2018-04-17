6e8ea3fc665b6a33c83de5766528fa27c72feda2
unknown <Cli@ENG-CHEN-LP.domain.local>
2018-04-17 10:45:21 -0500

    local dev

--- models/index.js
+++ models/index.js
@@ -4,14 +4,14 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development2';
+var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 //console.log(process.env.DATABASE_URL)
 /*
 var sequelize = new Sequelize(process.env.DATABASE_URL, {dialectOptions: {
-  ssl: true  //for SSL config since Heroku gives you this out of the box 
+  ssl: true  //for SSL config since Heroku gives you this out of the box
 }});
 */
 
